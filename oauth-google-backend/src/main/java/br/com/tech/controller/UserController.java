6ab426dc81ca98106720df6aca5ad11e6c151aff
Augusto Calisto <augustoaquino007@gmail.com>
2026-05-26 21:43:09 -0300

    Atualizando data ultimo login

--- oauth-google-backend/src/main/java/br/com/tech/controller/UserController.java
+++ oauth-google-backend/src/main/java/br/com/tech/controller/UserController.java
@@ -1,9 +1,10 @@
 package br.com.tech.controller;
 
-import java.util.List;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,9 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.tech.dto.AuthSuccessResponse;
-import br.com.tech.entity.Role;
 import br.com.tech.entity.User;
 import br.com.tech.service.UserService;
+import br.com.tech.util.UserUtil;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -44,21 +45,13 @@ public class UserController {
 		if(optional.isPresent()) {
 			User user = optional.get();
 			
-			List<String> userRoles = user.getRoles().stream()
-													.map(Role::getName)
-													.collect(Collectors.toList());
+			AuthSuccessResponse authSuccess = UserUtil.buildUserInfo(user, oidcUser, authorizedClient);
+						
+			LocalDate loginDate = user.getLastLoginDateTime().toLocalDate();
 			
-			AuthSuccessResponse authSuccess = AuthSuccessResponse
-												.builder()
-													.name(user.getName())
-													.email(user.getEmail())
-													.familyName(oidcUser.getClaim("family_name"))
-													.urlPicture(oidcUser.getClaim("picture"))
-													.providerName(providerName)
-													.token(oidcUser.getIdToken().getTokenValue())
-													.scopes(authorizedClient.getClientRegistration().getScopes())
-													.roles(userRoles)
-												.build();
+			if(loginDate.isBefore(LocalDate.now())) {
+				userService.updateLastLoginDate(user.getId(), LocalDateTime.now());
+			}
 						
 			return ResponseEntity.status(HttpStatus.OK).body(authSuccess);
 		}
